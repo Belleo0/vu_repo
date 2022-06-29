59badaecebbd6ec5cf3a33dcc93f8ae7de857a39
Lee Dokyung <dg.lee@rsad.co.kr>
2022-06-29 17:53:11 +0900

    fix:레미콘사-건설현장 맵 추가(퍼블리싱 안함)

--- src/pages/field-map/index.tsx
+++ src/pages/field-map/index.tsx
@@ -1,5 +1,237 @@
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import SpaceMapLayout from '@layout/SpaceMapLayout';
+import NaverMap from '@components/NaverMap';
+import NaverMapSpaceMarker from '@components/NaverMapSpaceMarker';
+
+import styled from '@emotion/styled';
+import SpaceMapSidebar from '@components/SpaceMapSidebar';
+import Footer from '@components/Footer';
+import useFactoryMaps from '@api/useFactoryMaps';
+import SpaceMarkerContent from '@components/SpaceMarkerContent';
+import getAssetURL from '@utils/getAssetURL';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import NaverMapPolyline from '@components/NaverMapPolyline';
+import { setPolylineInfo } from '@data/map';
+import NaverMapImageMarker from '@components/NaverMapImageMarker';
+import NaverMapDirectionMarker from '@components/NaverMapDirectionMarker';
+import DirectionMarkerContent from '@components/DirectionMarkerContent';
+import MapSpaceInfoModal from '@components/MapSpaceInfoModal';
+
+import { debounce } from 'lodash';
+import { usePrevious } from '@hooks/usePrevious';
+import NaverMapController from '@components/NaverMapController';
 
 export default () => {
-  return <SpaceMapLayout>{}</SpaceMapLayout>;
+  const dispatch = useDispatch();
+
+  const { polylineInfo } = useSelector((s: any) => s.map, shallowEqual);
+
+  const centerPaths = useMemo(() => {
+    if (!polylineInfo) return null;
+    return polylineInfo?.path[Math.floor(polylineInfo?.path.length / 2)];
+  }, [polylineInfo]);
+
+  const [duration, setDuration] = useState<string>('60');
+
+  const [selectedFieldInfo, setSelectedFieldInfo] = useState<any>(null);
+
+  const selectedFieldId = useMemo(() => {
+    if (!selectedFieldInfo) return null;
+    return selectedFieldInfo.id;
+  }, [selectedFieldInfo]);
+
+  const [bounds, setBounds] = useState<any>(null);
+
+  const [address, setAddress] = useState('');
+  const [realAddress, setRealAddress] = useState('');
+
+  const { data: factoriesData, isLoading } = useFactoryMaps(
+    selectedFieldId,
+    duration,
+    bounds,
+    realAddress,
+  );
+
+  const previousFactories = usePrevious(factoriesData);
+
+  const factories = useMemo(() => {
+    if (factoriesData) return factoriesData;
+    return previousFactories;
+  }, [factoriesData]);
+
+  const [selectedFactoryIds, setSelectedFactoryIds] = useState<number[]>([]);
+
+  const [selectedSpaceInfo, setSelectedSpaceInfo] = useState(null);
+
+  const [order, setOrder] = useState('거리순');
+
+  const delayedUpdateCall = useRef(debounce((q) => setBounds(q), 1000)).current;
+
+  const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
+
+  const handleClickFactoryCard = useCallback(
+    (id: number) => {
+      setSelectedFactoryIds((prev: number[]) => {
+        if (prev.includes(id)) {
+          const result = prev.filter((v) => v !== id);
+          return result;
+        } else {
+          const result = prev.concat(id);
+          return result;
+        }
+      });
+    },
+    [selectedFactoryIds],
+  );
+
+  useEffect(() => {
+    console.log('changed selectedFactoryIds', selectedFactoryIds);
+  }, [selectedFactoryIds]);
+
+  useEffect(() => {
+    if (selectedSpaceInfo) {
+      setAddress(selectedFieldInfo?.basic_address);
+    }
+  }, [selectedFieldInfo]);
+
+  useEffect(() => {
+    if (factoriesData?.message === '주소 위치를 찾을 수 없습니다.') {
+      window.alert('주소 위치를 찾을 수 없습니다.');
+      setRealAddress('');
+    }
+  }, [factoriesData]);
+
+  return (
+    <SpaceMapLayout>
+      <SpaceMapSidebar
+        factories={factories}
+        order={order}
+        setOrder={setOrder}
+        duration={duration}
+        setDuration={setDuration}
+        selectedFieldInfo={selectedFieldInfo}
+        setSelectedFieldInfo={setSelectedFieldInfo}
+        selectedFactoryIds={selectedFactoryIds}
+        selectedFieldId={selectedFieldId}
+        setSelectedSpaceInfo={setSelectedSpaceInfo}
+        setIsInfoModalOpen={setIsInfoModalOpen}
+        handleClickFactoryCard={handleClickFactoryCard}
+        address={address}
+        setAddress={setAddress}
+        setRealAddress={setRealAddress}
+      />
+      <ContentWrap>
+        <Content>
+          <NaverMap
+            lat={37.557733}
+            lng={126.9253985}
+            zoom={15}
+            style={{ width: '100%', height: 'calc(100vh - 80px)' }}
+            boundChange={({ _min, _max }) => {
+              const data = {
+                min_lat: _min._lat,
+                min_lng: _min._lng,
+                max_lat: _max._lat,
+                max_lng: _max._lng,
+              };
+              delayedUpdateCall(data);
+            }}
+          >
+            {polylineInfo?.path && (
+              <NaverMapPolyline paths={polylineInfo?.path} />
+            )}
+            {centerPaths && (
+              <NaverMapDirectionMarker
+                lat={centerPaths[1]}
+                lng={centerPaths[0]}
+                content={
+                  <DirectionMarkerContent
+                    distance={polylineInfo.distance}
+                    duration={polylineInfo.duration}
+                  />
+                }
+              />
+            )}
+            {!!factories?.field_position && (
+              <NaverMapImageMarker
+                lat={factories?.field_position.latitude}
+                lng={factories?.field_position.longitude}
+                content={
+                  <img
+                    src={getAssetURL('../assets/ic-field-marker.png')}
+                    style={{ width: 119, height: 72 }}
+                  />
+                }
+              />
+            )}
+
+            {factories &&
+              factories.data?.map((v: any, i: number) => (
+                <NaverMapSpaceMarker
+                  key={v.id}
+                  lat={v.latitude}
+                  lng={v.longitude}
+                  content={
+                    <SpaceMarkerContent
+                      index={i}
+                      name={v?.name}
+                      address={v?.basic_address}
+                      distance={v?.direction?.distance}
+                      duration={v?.direction?.duration}
+                      onClick={() => {
+                        if (selectedFieldId !== null) {
+                          handleClickFactoryCard(v.id);
+                        }
+                      }}
+                      onInfo={() => {
+                        setIsInfoModalOpen(false);
+                        setSelectedSpaceInfo(v);
+                        setTimeout(() => {
+                          setIsInfoModalOpen(true);
+                        }, 250);
+                      }}
+                      onChangePath={() => {
+                        dispatch(setPolylineInfo(v?.direction));
+                      }}
+                      selected={
+                        (!!v.direction && v.direction === polylineInfo) ||
+                        selectedFactoryIds.includes(v.id)
+                      }
+                      hideWithoutName={!!factories?.field_position}
+                    />
+                  }
+                />
+              ))}
+            <NaverMapController />
+          </NaverMap>
+          <MapSpaceInfoModal
+            open={isInfoModalOpen}
+            onClose={() => setIsInfoModalOpen(false)}
+            data={selectedSpaceInfo}
+          />
+        </Content>
+        <Footer />
+      </ContentWrap>
+    </SpaceMapLayout>
+  );
 };
+
+const ContentWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #fafafa;
+  overflow-y: scroll;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  position: relative;
+`;
