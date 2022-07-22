2cc32fe69049918371ab5c186673dd334bda792c
Lee Seungmin <lee@seungmin.io>
2022-07-22 11:44:07 +0900

    feat: 최대범위 조정

--- src/api/useFieldMaps.ts
+++ src/api/useFieldMaps.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 
 export default (areas: string, bounds: any, currentZoomLevel: number) => {
   const { data, error, mutate } = useSWR<any>([
-    currentZoomLevel >= 18 ? `/field-crawlings/maps` : null,
+    currentZoomLevel >= 16 ? `/field-crawlings/maps` : null,
     {
       areas,
       ...(bounds || {}),
